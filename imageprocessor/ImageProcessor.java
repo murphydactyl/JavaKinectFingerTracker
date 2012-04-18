9ed9fcb7836276f7ac9c2bf25569c68417158df5
Murphy Stein <murphy@wonderlab.com>
2012-04-18 13:12:46 -0400

    Added more comments

--- imageprocessor/ImageProcessor.java
+++ imageprocessor/ImageProcessor.java
@@ -1,3 +1,33 @@
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+//
+//  ImageProcessor.java
+//
+//  Utility functions for messing with images in real-time
+//
+//
+//  AUTHOR:   Murphy Stein
+//            New York University
+//  CREATED:  Jan 2012
+//
+//  LICENSE:  BSD
+//
+//  Copyright (c) 2012 New York University.
+//  All rights reserved.
+//
+//  Redistribution and use in source and binary forms are permitted
+//  provided that the above copyright notice and this paragraph are
+//  duplicated in all such forms and that any documentation,
+//  advertising materials, and other materials related to such
+//  distribution and use acknowledge that the software was developed
+//  by New York Univserity.  The name of the
+//  University may not be used to endorse or promote products derived
+//  from this software without specific prior written permission.
+//  THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+//  IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 package imageprocessor;
 
 import java.awt.*;
